79f8590fe6c373dd4ad8af7c211c5efebaea4ca8
chenyenting1998 <tumhark@gmail.com>
2023-06-27 17:58:44 +0800

    update sizedata description

--- R/sizedata.R
+++ R/sizedata.R
@@ -5,25 +5,31 @@
 #' @details  The condition and unit of each column were listed below.
 #'   \itemize{
 #'      \item{\code{Cruise}} The cruise during sampling.
-#'      \item{\code{Habitat}} Habitat type (Shelf, slope, canyon)
+#'      \item{\code{Habitat}} Habitat type (shelf, slope, canyon)
 #'      \item{\code{Station}} Sampling station.
 #'      \item{\code{Deployment}} Number of deployment.
 #'      \item{\code{Tube}} Order of tubes.
 #'      \item{\code{Section}} sampled section in cm
 #'      \item{\code{Taxon}} Recorded grouping taxa of the specimen
+#'      \item{\code{Family}} Recorded family of the specimen
+#'      \item{\code{Genus}} Recorded genus of the specimen
 #'      \item{\code{Condition}} The condition of the specimen
+#'      \item{\code{Type}} The respective geometric shapes for biovolume estimation
 #'      \item{\code{L}} maximum body length in mm
 #'      \item{\code{W}} maximum body length in mm
-#'      \item{\code{size}} the volume of the specimen in mm^3
-#'      \item{\code{Note}} Comments and further information for the specimen
-#'      \item{\code{Type}} The respective geometric shapes for biovolume estimation
 #'      \item{\code{C}} The conversion factors for biovolume estimation
-#'      \item{\code{WM}} wet mass in mg
+#'      \item{\code{Size}} The volume of the specimen in mm^3
+#'      \item{\code{WM}} Wet mass in mg; calculated from `Size` with the assumption
+#'                       of 1.13 specific gravity (Gerlach et al., 1985)
+#'      \item{\code{Note}} Comments and further information for the specimen
 #' }
+#' @references
 #'
 #' @name sizedata
 #' @keywords datasets
 NULL
 
 #' @rdname sizedata
-"size"
+"macrofauna_measurements"
+#' @rdname sizedata
+"macrofauna_biomass"
