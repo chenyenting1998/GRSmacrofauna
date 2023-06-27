45cf7a6db78c8f7afa75c4ea79aa40ff870203bf
chenyenting1998 <tumhark@gmail.com>
2023-06-27 15:04:48 +0800

    adjust description

--- data-raw/biovolume_method.R
+++ data-raw/biovolume_method.R
@@ -1,16 +1,24 @@
-#############################
-### Adding biovolume data ###
-#############################
+###########################
+### Adding biovolume data #
+###########################
+# Author: Yen-Ting Chen
+# Date of creation: unknown
+# Last date of modification: 2023/06/27
 
 # Description
 # Add biovolume estimation method for further biomass estimation
-# See Feller and Warwick (1988) for length-width relationships
-# and Hillebrand et al. (1999) for geometric estimation.
+# See Feller and Warwick (1988) for length-width relationships,
+# Hillebrand et al. (1999) for geometric estimation,
+# and Benoist et al., (2020) for generalized volumetric method.
 #
-# Reference:
+# Reference (reference style needs to be revised):
 # Feller RJ, Warwick RM (1988) Energetics. In: Introduction to the study  of meiofauna. Smithsonian Institution Press, p 181–196
 # Hillebrand H, Dürselen C-D, Kirschtel D, Pollingher U, Zohary T (1999) Biovolume calculation for pelagic and benthic microalgae. Journal of Phycology 35:403–424.
+# Benoist, N.M.A., Bett, B.J., Morris, K.J., Ruhl, H.A., 2019. A generalised volumetric method to estimate the biomass of photographically surveyed benthic megafauna. Progress in Oceanography 178, 102188. https://doi.org/10.1016/j.pocean.2019.102188
 
+################
+# Load packages
+################
 library(readxl)
 library(dplyr)
 library(usethis)
@@ -21,6 +29,6 @@ library(knitr)
 library(roxygen2)
 load_all()
 
-biovolume_method <- read_xlsx("data-raw/biovolume_method.xlsx")
+biovolume_type <- read_xlsx("data-raw/biovolume_type.xlsx")
 
-use_data(biovolume_method, overwrite = TRUE)
+use_data(biovolume_type, overwrite = TRUE)
