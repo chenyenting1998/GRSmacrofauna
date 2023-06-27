aca29d657b70dc12681fb12f8f95dc56662617cc
chenyenting1998 <tumhark@gmail.com>
2023-06-27 15:14:02 +0800

    add a new project_development file to store functions

---
+++ package_development.R
@@ -0,0 +1,37 @@
+######################################
+### GRSmacrofauna package development
+######################################
+# Author: Yen-Ting Chen
+# Date of creation: 2023/06/27
+# Last date of modification: 2023/06/27
+
+##############
+# Description
+##############
+# A list of packages and functions to rinse and repeat the
+# package development process.
+
+################
+# Load packages
+################
+library(readxl)
+library(dplyr)
+library(usethis)
+library(data.table)
+library(devtools)
+library(testthat)
+library(knitr)
+library(roxygen2)
+load_all() # load GRSmacrofauna
+
+
+############
+# Functions
+############
+# to load the package in del.
+load_all()
+# to quickly examine all aspects of the package
+check()
+# write functions in NAMESPACE
+document()
+#
