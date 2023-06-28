b018b464329ca0999936b9bd143a44b8864f3cd9
chenyenting1998 <tumhark@gmail.com>
2023-06-28 11:25:35 +0800

    update ctd ?help description

---
+++ R/ctd.R
@@ -0,0 +1,28 @@
+#' @title CTD downcast measurements
+#'
+#' @docType data
+#' @author Yen-Ting Chen \email{r08241220@@ntu.edu.tw}
+#' @details  The condition and unit of each column were listed below.
+#'   \itemize{
+#'      \item{\code{Cruise}} The cruise during sampling.
+#'      \item{\code{Habitat}} Habitat type (shelf, slope, canyon)
+#'      \item{\code{Station}} Sampling station.
+#'      \item{\code{Date}} Date recorded in YYYY-MM-DD.
+#'      \item{\code{Latitude}} Latitude in decimals.
+#'      \item{\code{Longitude}} Longitude in decimals.
+#'      \item{\code{Pressure}} Pressure in decibar.
+#'      \item{\code{Temperature}} Temperature in Celcius.
+#'      \item{\code{Salinity}} Salinity in Practical salinity unit (PSU).
+#'      \item{\code{SigmaTheta}} Potential density anomaly (Sigma-Theta) in kg/m^3.
+#'      \item{\code{Density}} Potential density in kg/m^3.
+#'      \item{\code{Oxygen}} Dissolved oxygen in mg/L.
+#'      \item{\code{Fluorescence}} Estimated chlorophyll concentration in mg/L.
+#'      \item{\code{Transmission}} Light transmission in percent.
+#' }
+#'
+#' @name ctd
+#' @keywords datasets
+NULL
+
+#' @rdname ctd
+"ctd"
