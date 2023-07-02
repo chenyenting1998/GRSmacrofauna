5f1c70b7acc48ca5dfa9e9e5056eca7f5a95dcb2
chenyenting1998 <tumhark@gmail.com>
2023-07-02 21:23:49 +0800

    OU data description

--- R/ou.R
+++ R/ou.R
@@ -1,17 +1,23 @@
-#' @title Oxygen utilization data
+#' @title Sediment oxygen community utilization data
 #'
 #' @docType data
 #' @author Yen-Ting Chen \email{r08241220@@ntu.edu.tw}
 #' @details  The condition and unit of each column were listed below.
 #'   \itemize{
 #'      \item{\code{Cruise}} The cruise during sampling.
+#'      \item{\code{Habitat}} Habitat type (shelf, slope, canyon)
 #'      \item{\code{Station}} Sampling station.
+#'      \item{\code{Date}} Date recorded in YYYY-MM-DD.
 #'      \item{\code{Deployment}} Number of deployment.
 #'      \item{\code{Tube}} Order of tubes.
-#'      \item{\code{TOU}} Total oxygen utilization of the sediment (mmol/m2/d).
-#'      \item{\code{DOU}} Dissolved oxygen utilzation of the sediment (mmol/m2/d).
-#'      \item{\code{BOU}} Benthic-meidated oxygen utilization of the sediment. Derived by subtracting TOU with DOU.
-#'      \item{\code{OPD}} Oxygen penetration depth (cm).
+#'      \item{\code{Channel}} Microelectrode channels; only in `dou` data set.
+#'      \item{\code{Incub_temperature}} Incubated water temperature in Celcius.
+#'      \item{\code{Incub_TOU}} Incubated total oxygen utilization in mmol/m^2/d^1.
+#'      \item{\code{Incub_DOU}} Incubated dissolved oxygen utilization in mmol/m^2/d^1.
+#'      \item{\code{In_situ_temperature}} Bottom water temperature in Celcius.
+#'      \item{\code{In_situ_TOU}} In situ total oxygen utilization in mmol/m^2/d^1.
+#'      \item{\code{In_situ_DOU}} In situ dissolved oxygen utilization in mmol/m^2/d^1.
+#'      \item{\code{OPD}} Oxygen penetration depth in cm.
 #' }
 #'
 #' @name ou
@@ -19,4 +25,6 @@
 NULL
 
 #' @rdname ou
-"ou"
+"tou"
+#' @rdname ou
+"dou"
