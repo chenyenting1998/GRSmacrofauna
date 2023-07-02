6d087810e8b603fcc61efc78c75484f601db4f05
chenyenting1998 <tumhark@gmail.com>
2023-07-02 21:23:27 +0800

    update oxygen utilization data curation

--- data-raw/TOUDOU.R
+++ data-raw/TOUDOU.R
@@ -1,86 +1,117 @@
 #################################
-### tou and dou data cleaning ###
+### TOU and DOU data cleaning ###
 #################################
+# Author: Yen-Ting Chen
+# Date of creation: unknown
+# Last date of modification: 2023/06/28
 
+##############
 # Description
+##############
 # Selecting tou and dou
 
+###################
+# Loading packages
+###################
 library(readxl)
 library(dplyr)
+
+# dev packages
 library(devtools)
 library(ggplot2)
 library(usethat)
 library(knitr)
 library(roxygen2)
-
 load_all()
 
-# read data
-dou <- read.csv("data-raw/GPSC_dou.csv")
-tou <- read.csv("data-raw/GPSC_tou.csv")
+###################
+# Set up functions
+###################
+clean_data <- function(data){
+  data_cleaned <-
+    data %>%
+    filter(Cruise %in% c("OR1_1219", "OR1_1242")) %>%
+    filter(!Station %in% c("GC1", "GS1")) %>%
+    mutate(Cruise = gsub("_", "-", Cruise)) %>%
+    mutate(Deployment = as.numeric(Deployment)) %>%
+    mutate(Tube = as.numeric(Tube))
+  return(data_cleaned)
+}
 
-# tou
-tou$Cruise <- gsub("_", "-", tou$Cruise)
-tou$Deployment <- as.character(tou$Deployment)
-tou$Tube <- as.character(tou$Tube)
+###############
+# 1. Load data
+###############
+tou_raw <- read.csv("data-raw/GPSC_tou.csv")
+dou_raw <- read.csv("data-raw/GPSC_dou.csv")
+
+######################
+# 2. Process TOU data
+######################
+# change column name and standardize units
 tou <-
-  tou %>%
-  filter(Cruise %in% c("OR1-1219", "OR1-1242")) %>%
-  filter(!Station %in% c("GC1", "GS1")) %>%
-  mutate(TOU = - In_situ_DO_flux) %>%
-  select(Cruise, Station, Deployment, Tube, TOU)
+  clean_data(tou_raw) %>%
+  # changing column names and values of DO flux
+  mutate(Incub_temperature = Temperature) %>%
+  mutate(Incub_TOU = - DO_flux) %>%
+  mutate(In_situ_temperature = In_situ_temperature) %>%
+  mutate(In_situ_TOU = - In_situ_DO_flux) %>%
+  select(Cruise, Habitat, Station, Date, Deployment, Tube,
+         Incub_temperature,
+         Incub_TOU,
+         In_situ_temperature,
+         In_situ_TOU)
 
-ggplot(tou)+
-  geom_bar(aes(x = Station, y = TOU, fill = Tube),
-           stat = "identity",
-           position = "dodge")+
-  facet_wrap(~Cruise, scales = "free_x")+
+# just some figures
+# incub TOU
+ggplot(tou_raw[tou_raw$Cruise %in% c("OR1_1219", "OR1_1242"),])+
+  geom_point(aes(x = Temperature, y = - DO_flux, color = Cruise))+
   theme_bw()
 
+ggplot(tou_raw[tou_raw$Cruise %in% c("OR1_1219", "OR1_1242"),])+
+  geom_point(aes(x = Depth, y = - DO_flux, color = Cruise))+
+  theme_bw()
 
-# dou
-dou$Cruise <- gsub("_", "-", dou$Cruise)
-dou$Deployment <- as.character(dou$Deployment)
-dou$Tube <- as.character(dou$Tube)
+# in situ TOU
+ggplot(tou_raw[tou_raw$Cruise %in% c("OR1_1219", "OR1_1242"),])+
+  geom_point(aes(x = In_situ_temperature, y = - In_situ_DO_flux, color = Cruise))+
+  theme_bw()
 
+ggplot(tou_raw[tou_raw$Cruise %in% c("OR1_1219", "OR1_1242"),])+
+  geom_point(aes(x = Depth, y = - In_situ_DO_flux, color = Cruise))+
+  theme_bw()
+
+######################
+# 3. Process DOU data
+######################
+# change column name and standardize units
 dou <-
+  clean_data(dou_raw) %>%
+  # change column name
+  mutate(Incub_temperature = Temperature) %>%
+  # change DOU unit
+  mutate(Incub_DOU = - Integrated_Prod / 1000000 * 10000 * 60 * 60 * 24) %>%
+  mutate(In_situ_DOU = - In_situ_Integrated_Prod / 1000000 * 10000 * 60 * 60 * 24) %>%
+  #                                                mmol      m-2     day-1
+  # change OPD unit
+  mutate(OPD = OPD / 10000) %>%  # um to cm
+  select(Cruise, Habitat, Station, Deployment, Tube, Channel,
+         Incub_temperature, Incub_DOU,
+         In_situ_temperature, In_situ_DOU,
+         OPD)
+
+dou_averaged <-
   dou %>%
-  filter(Cruise %in% c("OR1-1219", "OR1-1242")) %>%
-  filter(!Station %in% c("GC1", "GS1")) %>%
-  mutate(DOU = In_situ_Integrated_Prod / 1000000 * 10000 * 60 * 60 * 24) %>%
-  #                                      mmol      m-2     day-1
-  group_by(Cruise, Station, Deployment, Tube) %>%
-  summarize(DOU = mean(abs(DOU)),
-            OPD = mean(OPD/10000)) # um to cm
+  group_by(Cruise, Habitat, Station, Deployment, Tube) %>%
+  # taking the average of temperature is just suppress an error
+  # there is only one temperature probe
+  summarise(Incub_temperature = mean(Incub_temperature),
+            Incub_DOU = mean(Incub_DOU),
+            In_situ_temperature = mean(In_situ_temperature),
+            In_situ_DOU = mean(In_situ_DOU),
+            OPD = mean(OPD))
 
-ggplot(dou)+
-  geom_bar(aes(x = Station, y = DOU, fill = Tube),
-          stat = "identity",
-          position = "dodge")+
-  facet_wrap(~Cruise, scales = "free_x")+
-  theme_bw()
-
-# merge tou and dou data
-tou %>%
-  left_join(dou) %>%
-  mutate(BOU = TOU - DOU) %>%
-  ggplot()+
-  geom_bar(aes(x = Station, y = TOU, fill = Tube),
-           stat = "identity",
-           position = "dodge") +
-  geom_point(aes(x = Station, y = DOU, fill = Tube),
-           stat = "identity",
-           position = "dodge") +
-  facet_wrap(~Cruise, scale = "free_x") +
-  theme_bw()
-
-ou <-
-  left_join(tou, dou) %>%
-  mutate(BOU = TOU-DOU) %>%
-  relocate(BOU, .after = DOU)
-str(ou)
-
-ou$Deployment <- as.double(ou$Deployment)
-ou$Tube <- as.double(ou$Tube)
-# output data
-use_data(ou, overwrite = TRUE)
+############
+# 4. Output
+############
+use_data(tou, overwrite = TRUE)
+use_data(dou, overwrite = TRUE)
