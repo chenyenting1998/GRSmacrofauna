ea8b48b7483b6e40ef3290ea5a3c0861859e9ef2
Yen-Ting Chen <yenting.chen1998@gmail.com>
2023-10-20 23:06:28 +0200

    update typhoontrack date format

--- data-raw/typhoon_track.R
+++ data-raw/typhoon_track.R
@@ -3,10 +3,9 @@
 ###################################
 # Author: Yen-Ting Chen
 # Date of creation: 2023/10/17
-# Last date of modification: 2023/10/17
+# Last date of modification: 2023/10/20
 
 library(usethis)
-library(lubridate)
 # set path
 path <- unz("data-raw/2019.TrackData.zip", "2019_TrackData/2019.BAILU.TrackBEST.txt")
 # read data
@@ -17,9 +16,10 @@ colnames(data) <- c("Year", "Name", "Date", "Time",
                     "Gust", "7Dir", "10Dir", "Warn")
 
 # format time
-data$Date <- ymd(data$Date)
-data$Time <- hm(data$Time)
-
+data$Date <- as.POSIXlt(data$Date)
+data$Time <- as.POSIXlt(data$Time)
+class(data$Date)
+class(data$Time)
 # rename data
 typhoontrack<- data
 
